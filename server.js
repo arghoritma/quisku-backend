ee81f3b931b128db03182007f3e895b6f7058b31
rozak <bigzero55@outlook.com>
2024-12-29 02:34:27 +0700

    prod

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 require("dotenv").config();
 const { initializeDatabase } = require("./utils/initilaDatabase");
-const HOST = process.env.HOST;
-const PORT = process.env.PORT;
+const HOST = process.env.HOST || "localhost";
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
